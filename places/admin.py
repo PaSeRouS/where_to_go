6a9f6a39c54dc7d66f616e62884a1e7d94c200d4
PaSeRouS <paser96@mail.ru>
2022-08-19 20:27:50 +0300

    В админке добавлен предпросмотр загруженных фотографий

--- places/admin.py
+++ places/admin.py
@@ -1,14 +1,30 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
 from .models import Image, Place
 
 class ImageInline(admin.TabularInline):
     model = Image
     extra = 0
+    fields = ('image', 'get_preview', 'image_id')
+
+    readonly_fields = ['get_preview']
+
+    def get_preview(self, obj):
+        prop_height = 200
+        prop_width = prop_height * obj.image.width / obj.image.height
+
+        return format_html('<img src="{}" width="{}" height={} />',
+            obj.image.url,
+            prop_width,
+            prop_height,
+        )
+
 
 @admin.register(Image)
 class ImageAdmin(admin.ModelAdmin):
     pass
+
 
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
